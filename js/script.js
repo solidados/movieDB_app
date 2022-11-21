a589a115e9b30c65424c65565325bcf67c1d3fd0
solidados <geekbrains.pk@internet.ru>
2022-11-22 00:09:23 +0400

    fix: buttons and input on page

--- js/script.js
+++ js/script.js
@@ -93,9 +93,9 @@ const movieDB = {
 };
 
 const adv = document.querySelectorAll('.promo__adv img'),
-      poster = document.querySelector('.promo__bg'),
-        genre = poster.querySelector('.promo__genre');
-      movieList = document.querySelector('.promo__interactive-list');
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre');
+movieList = document.querySelector('.promo__interactive-list');
 
 adv.forEach(item => {
     item.remove();
